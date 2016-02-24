497dab3396ad51059f0eaf15cef9c314adbdcffd
Jorden Hill <jhill4756@gmail.com>
2016-02-23 20:16:13 -0500

    Set up basis for Swift Word2Vec
    Set up a lot of the basic structure for Word2Vec. Need to finish system
    and create tests.

--- Birdbrain/MyPlayground.playground/Contents.swift
+++ Birdbrain/MyPlayground.playground/Contents.swift
@@ -1,5 +1,7 @@
 //A test playground containing various bits of quick "research"
 
+import Foundation
+
 //Size of Ints
 sizeof(Int)
 sizeof(Int32)
@@ -9,3 +11,33 @@ sizeof(Int16)
 
 var string = "Hello"
 string.characters.count
+
+//Checking size 
+sizeof(Int) * 100000000
+
+//C Strings in Swift
+var char = "a"
+let p = "string"
+var ccharoptional = char.cStringUsingEncoding(NSUTF8StringEncoding)![0]
+var sum = 0
+for a in 0..<p.characters.count {
+  print(Int(String(p[p.startIndex.advancedBy(a)]).cStringUsingEncoding(NSUTF8StringEncoding)![0]))
+  sum += Int(String(p[p.startIndex.advancedBy(a)]).cStringUsingEncoding(NSUTF8StringEncoding)![0])
+  print(sum)
+}
+sum
+
+//Messing around with sorting arrays and a struct
+struct thing {
+  var val = Int()
+}
+
+var array = [thing]()
+for i in 0..<10 {
+  array.append(thing(val: i))
+}
+
+var firstElement = array[0]
+var sortedArray = array.dropFirst().sort() { a,b in return a.val < b.val}
+sortedArray.insert(firstElement, atIndex: 0)
+array = sortedArray
--- Birdbrain/Word2Vec.swift
+++ Birdbrain/Word2Vec.swift
@@ -6,27 +6,45 @@
 //  Copyright © 2016 Jorden Hill. All rights reserved.
 //
 
-/*import Foundation
+import Foundation
+
+struct vocabWord {
+  var cn: Int //The count of occurences of the word
+  var point: [Int]
+  var word: String //The word
+  var code: [Int8]
+  var codeLength: Int8
+  
+  init() {
+    cn = 0
+    point = [0]
+    word = ""
+    code = [0]
+    codeLength = 0
+  }
+}
 
 let maxString = 100
 let expTableSize = 1000
-let maxExp = 6
+let maxExp: Float = 6
 let maxSentenceLength = 1000
 let maxCodeLength = 40
 let vocabHashSize = 30000000;
 
 var vocab = [vocabWord]()
 var binary = 0, cbow = 1, debugMode = 2, window = 5, minCount = 5, numThreads = 12, minReduce = 1
-var vocabHash = Int()
+var vocabHash = [Int](count: vocabHashSize, repeatedValue: 0)
+
 var vocabMaxSize = 1000
 var vocabSize = 0
 var layer1Size = 100
 var trainWords = 0, wordCountActual = 0, iter = 5, fileSize = 0, classes = 0
 var alpha: Float = 0.025, startingAlpha = Float(), sample: Float = 0.001
-var syn0 = Float(), syn1 = Float(), syn1Neg = Float(), expTable = Float()
+var syn0 = Float(), syn1 = Float(), syn1Neg = Float()
+var expTable = [Float](count: expTableSize, repeatedValue: 0.0)
 
-var hs = 0, negative = 5;
-var tableSize = Int(1e8);
+var hs = 0, negative = 5
+var tableSize = 100000000
 var table = [Int]();
 
 /*
@@ -42,31 +60,68 @@ var table = [Int]();
  clock_t start;
 */
 
-struct vocabWord {
-  var cn: Int
-  var point: Int16
-  var word: String
-  var code: String
-  var codeLength: String
+/** The main method of Word2Vec, runs the system and trains the model on the words.
+ */
+func run() {
+  vocab = [vocabWord](count: vocabSize, repeatedValue: vocabWord())
+  vocabHash = [Int](count: vocabHashSize, repeatedValue: 0)
+  expTable = [Float](count: expTableSize + 1, repeatedValue: 0.0)
+  
+  for a in 0 ..< expTableSize {
+    expTable[a] = expf(Float((a / expTableSize * 2 - 1)) * maxExp)
+  }
+  
+  //Used Accelerate here because it could be a bit faster
+  expTable = div(expTable, y: add(expTable, c: 1.0))
+  
+  trainModel()
+}
+
+func trainModel() {
+  
 }
 
 func initalizeUnigramTable() {
-  var a: Int, i: Int
+  var i = 0
   var trainWordsPow: Double = 0
-  let d1: Double, power = 0.75
+  var d1 = 0.75, power = 0.75
   
   table = [Int](count: tableSize, repeatedValue: 0)
   
   for a in 0..<vocabSize {
-    trainWordsPow += pow(vocab[a], power)
+    trainWordsPow += pow(Double(vocab[a].cn), power)
+  }
+ 
+  d1 = pow(Double(vocab[i].cn), power) / trainWordsPow
+ 
+  for a in 0..<tableSize {
+    table[a] = i
+    if (Double(a / tableSize) > d1) {
+      i += 1
+      d1 += pow(Double(vocab[i].cn), power) / trainWordsPow
+    }
+    if (i >= vocabSize) {
+      i = vocabSize
+    }
   }
 }
+
+/** Compute the hash value of a word.
+  - Parameter: The word whose hash is to be found.
+  - Returns: An Int value that is the hash of the word.
+*/
+func getWordHash(word: String) -> Int {
+  var hash = 0
   
-func readWord(word: String, file: NSData) {
+  for character in 0 ..< word.characters.count {
+    hash = hash * 257 + Int(String(word[word.startIndex.advancedBy(character)]).cStringUsingEncoding(NSUTF8StringEncoding)![0])
+  }
   
+  return hash
 }
 
-// Adds a word to the vocabulary
+//** Adds a word to the vocabulary
+   
 func addWordToVocab(word: String) -> Int {
   var hash: Int = word.characters.count + 1, length: Int = word.characters.count + 1;
   if (length > maxString) {
@@ -75,7 +130,7 @@ func addWordToVocab(word: String) -> Int {
   
   vocab[vocabSize].word = word
   vocab[vocabSize].cn = 0;
-  vocabSize++;
+  vocabSize += 1;
   
   //TODO: Determine if necessary
   
@@ -87,11 +142,158 @@ func addWordToVocab(word: String) -> Int {
   
   hash = getWordHash(word);
   
-  while (vocab_hash[hash] != -1) {
+  while (vocabHash[hash] != -1) {
     hash = (hash + 1) % vocabHashSize;
   }
   
   vocabHash[hash] = vocabSize - 1;
   
   return vocabSize - 1;
-}*/
+}
+
+/** Find the position of a word in the vocabulary.
+  - Parameter word: the word to be found.
+  - Returns: -1 if the word was not found, otherwise returns the position of the word.
+*/
+func searchVocab(word: String) -> Int {
+  var hash = getWordHash(word)
+  
+  while (true) {
+    if (vocabHash[hash] == -1) {
+      return -1
+    }
+    if (word == vocab[vocabHash[hash]].word) {
+      return vocabHash[hash]
+    }
+    hash = (hash + 1) % vocabHashSize
+  }
+}
+
+/** Sort the vocuabulary by frequency using their word count values.
+*/
+func sortVocab() {
+  var size: Int
+  
+  //Sort array, keeping </s> as first element
+  let firstVocabElement = vocab.removeAtIndex(0)
+  vocab.sortInPlace() {word1, word2 in word1.cn < word2.cn}
+  vocab.insert(firstVocabElement, atIndex: 0)
+  
+  for a in 0..<vocabHashSize {
+    vocabHash[a] = -1
+  }
+  
+  size = vocabSize
+  trainWords = 0
+  
+  for b in 0..<size {
+    if ((vocab[b].cn < minCount) && (b != 0)) {
+      vocabSize -= 1
+    } else {
+      // recompute hash
+      var hash = getWordHash(vocab[b].word);
+      while (vocabHash[hash] != -1) {
+        hash = (hash + 1) % vocabHashSize
+      }
+      vocabHash[hash] = b;
+      trainWords += vocab[b].cn;
+    }
+  }
+  
+  shrinkVocab(&vocab)
+  
+  for c in 0..<vocabSize {
+    vocab[c].code = [Int8](count: maxCodeLength, repeatedValue: 0)
+    vocab[c].point = [Int](count: maxCodeLength, repeatedValue: 0)
+  }
+}
+
+/** Drop the early words until the current vocabulary size matches the new vocabulary size.
+  - Parameter vocab: The array containing the vocab words
+ */
+func shrinkVocab(inout vocab: [vocabWord]) {
+  var i = 1
+  
+  while (vocab.count > vocabSize) {
+    vocab.removeAtIndex(i)
+    i += 1
+  }
+}
+
+/** Create a binary huffman tree
+*/
+func createBinaryTree() {
+  var code = [Int8](count: maxCodeLength, repeatedValue: 0)
+  var point = [Int](count: maxCodeLength, repeatedValue: 0)
+  var count = [Int](count: vocabSize * 2 + 1, repeatedValue: 0)
+  var binary = [Int](count: vocabSize * 2 + 1, repeatedValue: 0)
+  var parentNode = [Int](count: vocabSize * 2 + 1, repeatedValue: 0)
+  var min1i: Int
+  var min2i: Int
+  
+  for a in 0 ..< vocabSize {
+    count[a] = vocab[a].cn
+  }
+  
+  for b in 0 ..< vocabSize * 2 {
+    count[b] = Int(1e15)
+  }
+  
+  var pos1 = vocabSize - 1
+  var pos2 = vocabSize
+  
+  for c in 0 ..< vocabSize {
+    // First, find two smallest nodes 'min1, min2'
+    if (pos1 >= 0) {
+      if (count[pos1] < count[pos2]) {
+        min1i = pos1;
+        pos1 -= 1
+      } else {
+        min1i = pos2;
+        pos2 += 1
+      }
+    } else {
+      min1i = pos2
+      pos2 += 1
+    }
+    if (pos1 >= 0) {
+      if (count[pos1] < count[pos2]) {
+        min2i = pos1
+        pos1 -= 1
+      } else {
+        min2i = pos2
+        pos2 += 1
+      }
+    } else {
+      min2i = pos2
+      pos2 += 1
+    }
+    count[vocabSize + c] = count[min1i] + count[min2i]
+    parentNode[min1i] = vocabSize + c
+    parentNode[min2i] = vocabSize + c
+    binary[min2i] = 1
+  }
+  
+  // Now assign binary code to each vocabulary word
+  for d in 0 ..< vocabSize {
+    var b = d
+    var i = 0
+    
+    while (true) {
+      code[i] = Int8(binary[b])
+      point[i] = b
+      i += 1
+      b = parentNode[b];
+      if (b == vocabSize * 2 - 2) {
+        break
+      }
+    }
+    vocab[d].codeLength = Int8(i);
+    vocab[d].point[0] = vocabSize - 2;
+    
+    for e in 0 ..< i {
+      vocab[d].code[i - e - 1] = code[e];
+      vocab[d].point[i - e] = point[e] - vocabSize;
+    }
+  }
+}
